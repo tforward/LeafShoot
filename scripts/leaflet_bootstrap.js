11035ae9702e3ec7334d6e097f27e9dd805fb446
tforward <tforward@gmail.com>
2017-03-16 21:06:30 -0400

    added files8

--- scripts/leaflet_bootstrap.js
+++ scripts/leaflet_bootstrap.js
@@ -3,8 +3,8 @@
 "use strict";
 
 var map;
-var lat = 51.059174; 
-var lng = -115.772533;
+var lat = 50.7765; 
+var lng = -116.8502;
 var zoom = 11;
 var geojson;
 var lastClickedLayer;
@@ -18,6 +18,8 @@ var OpenStreetMap_BaW = L.tileLayer("http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/
 function initmap() {
     // set up the map
     map = new L.Map("map");
+    
+    
 
     // Lock the map
     // var maxBoundsSouthWest = new L.LatLng(50.665131428416146, -114.62310791015625);
@@ -35,7 +37,7 @@ function initmap() {
     // start the map in Kingston
     map.setView(new L.LatLng(lat, lng), zoom);
     map.addLayer(Stamen_Terrain);
-
+    
     
     // map.locate({setView: true, maxZoom: 16});
 }
