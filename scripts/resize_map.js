105cd0e533e158257e5076501382f3a0345c66e6
tforward <tforward@gmail.com>
2017-03-19 22:20:08 -0400

    mobile fixes for if to small

--- scripts/resize_map.js
+++ scripts/resize_map.js
@@ -11,19 +11,18 @@ $(window).bind("resize", resize);
 resize();
 
 function resize(){
-    // Set the map size cannot be smaller than the width on mobile devices.
-    minMap = 415;
-    if($(window).width() <= minMap){
-        if($(window).height() <= minMap){
-            $('#map').css("height", resize_map.minHeight);
-        }else{
-            $('#map').css("height", ($(window).height()));
-        }
-    }else{
+    if($(window).width() >= resize_map.minHeight){
         $('#map').css("height", ($(window).height() - resize_map.mapmargin));
         // setTimeout, to Handle using the Maximize button which needs more time to respond correctly
         setTimeout(function() {
             $('#map').css("height", ($(window).height() - resize_map.mapmargin));
           }, 150);
+    // Set the map size cannot be smaller than the width on mobile devices.
+    }else{
+        if($(window).height() <= resize_map.minHeight){
+            $('#map').css("height", resize_map.minHeight);
+        }else{
+            $('#map').css("height", ($(window).height() - resize_map.mapmargin));
+        }
     }
 }
